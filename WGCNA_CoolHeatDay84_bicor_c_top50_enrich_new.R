c539f450faa1d17dcc869b4decba2e2a00e0cf86
Lihe Liu <liheliu@Lihes-MacBook-Pro.local>
2019-08-27 01:29:11 -0400

    re-run enrich in top 50 percent

--- WGCNA_CoolHeatDay84_bicor_c_top50_enrich_new.R
+++ WGCNA_CoolHeatDay84_bicor_c_top50_enrich_new.R
@@ -5,30 +5,60 @@ source("Functions_Source.R")
 #================================================================================================
 ###                                       1. dataprep                                      ######
 #================================================================================================
-load("networkData14prepare with corrections_top20.RData")
-load('AllColumnsLables.RData')
-networkData14_final <- networkData_final
-datExpr14_cl = t(networkData14_final[,c(1:(.5*ncol(networkData14_final)))])
-datExpr14_ht = t(networkData14_final[,c(((.5*ncol(networkData14_final)+1)):(ncol(networkData14_final)))])
+## read
+#setwd("/Users/liulihe95/Desktop/HeatStress0708");getwd()
+options(stringsAsFactors = FALSE)
+enableWGCNAThreads()
+CowsID_ht = c("6334","8514","8971","8867","8841","8966")# ID of heat group
+CowsID_cl = c("8252","8832","8896","8983","8897","8862")# ID of cool group
+# (all the IDs) 27570 genes; 12 cows; 3 time points 
+networkData = read.csv("count_matrix.tsv",sep = "\t",header = T);rownames(networkData) = networkData[,1];networkData = networkData[,-1] 
+dim(networkData)
+## find index 
+# day 14
+names14_last3 = substr(names(networkData), nchar(names(networkData)[2])-2, nchar(names(networkData)[2])); pos14 = grep(".14",names14_last3)
+names14 = names(networkData)[pos14];pos14_ht = substr(names14,2,5) %in% CowsID_ht;pos14_cl = substr(names14,2,5) %in% CowsID_cl
+# day 42
+names42_last3 = substr(names(networkData), nchar(names(networkData)[2])-2, nchar(names(networkData)[2])); pos42 = grep(".42",names42_last3)
+names42 = names(networkData)[pos42];pos42_ht = substr(names42,2,5) %in% CowsID_ht;pos42_cl = substr(names42,2,5) %in% CowsID_cl
+# day 84
+names84_last3 = substr(names(networkData), nchar(names(networkData)[2])-2, nchar(names(networkData)[2])); pos84 = grep(".84",names84_last3)
+names84 = names(networkData)[pos84];pos84_ht = substr(names84,2,5) %in% CowsID_ht;pos84_cl = substr(names84,2,5) %in% CowsID_cl
+#compose index
+column_14_cl = pos14[pos14_cl];column_14_ht = pos14[pos14_ht]
+column_42_cl = pos42[pos42_cl];column_42_ht = pos42[pos42_ht]
+column_84_cl = pos84[pos84_cl];column_84_ht = pos84[pos84_ht]
+# -4 in day14; -4 in day 42; -7 in day84 (think about which one to respect)
+networkData14 = networkData[,c(column_14_cl,column_14_ht)]
+networkData42 = networkData[,c(column_42_cl,column_42_ht)]
+networkData84 = networkData[,c(column_84_cl,column_84_ht)]
+########################################################################################################################
+networkData14_4pre = DataPre_C(networkData84, cousin = 0.4, 6, 6, 0.5)
+networkData14_final = networkData14_4pre$Corrected_log2_PC
+
+# step 6 - select into two groups
+datExpr14_cl = t(networkData14_final[,colnames(networkData14_final) %in% names(networkData)[column_84_cl] ])
+datExpr14_ht = t(networkData14_final[,colnames(networkData14_final) %in% names(networkData)[column_84_ht] ])
 datExpr14_cl = data.frame(datExpr14_cl);datExpr14_ht = data.frame(datExpr14_ht)
-dim(datExpr14_cl);dim(datExpr14_ht)
-print("datapre done!")
+
 #================================================================================================
 ###                                  2. weighted in day 14                                ######    
 #================================================================================================
 ## pick soft thresholds
 # Choose a set of soft-thresholding powers
-# Mean connectivity as a function of the soft-thresholding power
-#save(sft_b_cl,softPower_b,MeanK_b,file = "SoftThres_bicor_c_day14.RData")
+# Plot the results of threshold picking:
+#save
+#save(sft_b_cl,softPower_b,MeanK_b,file = "SoftThres bicor.RData")
 print("Step2 - soft thre plotted and Rdata saved")
 #==============================================================================================
 #                                 3. soft-threshold and dissimilarity                    ######
 #==============================================================================================
 # ref - cl; test - ht
 # dim(datExpr14_ht);dim(datExpr14_cl)
+# Peaerson Cor
 # save the matrix
-#save(adjacency14_b_cl,dissTOM14_b_cl,geneTree14_b_cl,adjacency14_b_ht,dissTOM14_b_ht,geneTree14_b_ht,file = "AllMatrixDay14_bicor_c.RData")
-#load("AllMatrixDay14_bicor_c.RData")
+#save(adjacency14_b_cl,dissTOM14_b_cl,geneTree14_b_cl,adjacency14_b_ht,dissTOM14_b_ht,geneTree14_b_ht,file = "AllMatrixday14 bicor.RData")
+load("AllMatrixday14 bicor.RData")
 print("Step3 - adj matrix created and rdata saved")
 #==========================================================================================
 #                                    4.  plot trees                                  ######
@@ -38,18 +68,13 @@ print("Step4 - dissmi plottd and rdata saved")
 #=========================================================================================
 #                                5.cutting and merging                              ######
 #=========================================================================================
-# set the minimum module size relatively high:
-print("Step5 - cutting finished")
-### Merging of modules whose expression profiles are very similar
-# Calculate eigengenes
-# Save module colors and labels for use in subsequent parts
-#save(MEs14_b_cl, moduleLabels14_b_cl, moduleColors14_b_cl, geneTree14_b_cl, file = "CoolHeatDay_bicor_c_day14.RData")
-load("CoolHeatDay_bicor_c_day14.RData")
-
+#save(MEs14_b_cl, moduleLabels14_b_cl, moduleColors14_b_cl, geneTree14_b_cl, file = "CoolHeatday14 bicor.RData")
+load("CoolHeatday14 bicor.RData")
 print("Step5 - mergeing finished")
 #=================================================================================================
 #                              6. plotting heatmap                                            ###
 #=================================================================================================
+print("Step6 - heapmap created")
 #===============================================================================================
 #                           7. plot cross-condition dendrogram                               ###
 #===============================================================================================
@@ -59,13 +84,12 @@ print("Step7 - cross condition dendrogram created")
 #=================================================================================================
 ######### To quantify this result module preservation statistics ######################
 # data pre and check data structure
-#save(mp14_b, file = "CoolHeatDay14_modulePreservation_bicor_c.RData")
-load("CoolHeatDay14_modulePreservation_bicor_c.RData")
-#load("CoolHeatDay14_modulePreservation_bicor_c.RData")
+dim(datExpr14_ht);dim(datExpr14_cl)
+# save(mp14_b, file = "CoolHeatDay14_modulePreservation bicor.RData")
+load("CoolHeatDay14_modulePreservation bicor.RData")
 # load("CoolHeatDay14_modulePreservation.RData")
 print("Step8 - mp finished and data saved")
 ################ output - shortest - only p summmary  ######################
-#write.csv(Results_b_mp14_1,"module_size_and_preservation_statistics_bicor_day14.csv")
 ################ output - shortest - only p summmary  ######################
 # specify the reference and the test networks
 ref=1; test = 2
@@ -81,8 +105,6 @@ Z.PreservationStats14_b=mp14_b$preservation$Z[[ref]][[test]]
 # Look at the observed preservation statistics
 # Obs.PreservationStats
 # Z statistics from the permutation test analysis Z.PreservationStats
-#write.csv(Obs.PreservationStats14_b,"Obs.PreservationStats_bicor_c_day14.csv")
-#write.csv(Z.PreservationStats14_b,"Z.PreservationStats_bicor_c_day14.csv")
 print("Step8 - preservation statistics calculated and saved")
 #===========================================================================================
 #                                9. mp visualization                                      ##
@@ -99,6 +121,7 @@ point.label14_b = modColors14_b[selectModules]
 medianRank14_b=Obs.PreservationStats14_b$medianRank.pres
 Zsummary14_b=Z.PreservationStats14_b$Zsummary.pres
 #
+#pdf("medianRank_Zsummary versus module size bicor.pdf",height = 8, width = 16)
 print("Step9 - preservation statistics calculated vis and saved")
 ############################################################################################################################
 ####### ###### ######   present individual Z  ###### ###### ###### ###### ###### 
@@ -111,15 +134,7 @@ moduleSizes14_b = mp14_b$quality$Z[[ref]][[test]][,1];
 plotMods = !(modColors14_b %in% c("grey", "gold"));
 # Create numeric labels for each module
 labs14_b= match(modColors14_b[plotMods], standardColors());
-
 # Compare preservation to quality:
-# Text labels for points
-#text = modColors14_b[plotMods];
-# Auxiliary convenience variable
-#plotData_b = cbind(mp14_b$preservation$observed[[ref]][[test]][,2], mp14_b$preservation$Z[[ref]][[test]][,2])
-# Main titles for the plot
-# Start the plot: open a suitably sized graphical window and set sectioning and margins.
-# Plot each Z statistic in a separate plot.
 print("Step9 - all_module_preservation_statistics finished and data saved")
 
 #===========================================================================================
@@ -135,7 +150,7 @@ Kegg_Enrichment_Results = Kegg_Enrich_Plot(ENS_ID_all,
                                            KEGGthres = 0.10,
                                            TestingGroupAssignment = moduleColors14_b_cl, 
                                            TestingSubsetNames = nonpres_modulenames_b,
-                                           keyword = "KEGG_Enrichment_Day14_bicor_c_new")
+                                           keyword = "KEGG_Enrichment_Day84_bicor_c_top50_new")
 #===========================================================================================
 #                             11. Gene Ontology enrichment                                ##
 #===========================================================================================
@@ -145,6 +160,5 @@ GO_Enrichment_Results = Go_Enrich_Plot(total.genes,
                                        GOthres = 0.05,
                                        TestingGroupAssignment = moduleColors14_b_cl,
                                        TestingSubsetNames = nonpres_modulenames_b,
-                                       keyword = "GO_Enrichment_Day14_bicor_c_new")
-
-
+                                       keyword = "GO_Enrichment_Day84_bicor_c_top50_new")
+print("Step11 - GO finished and data saved")
